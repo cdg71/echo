1b8a056b0fded6f67401e2dbcaeaf171fea91a2b
jpbourgeon <3356641+jpbourgeon@users.noreply.github.com>
2024-05-25 22:11:19 +0200

    fix: stream heartbeat component call the right /heartbeat route

--- src/services/streams/components/heartbeat.tsx
+++ src/services/streams/components/heartbeat.tsx
@@ -1,6 +1,6 @@
 export const heartbeatComponent = (streamId: string) => (
   <div
-    hx-get={`/keepalive/${streamId}`}
+    hx-get={`/heartbeat/${streamId}`}
     hx-trigger={`load delay:${import.meta.env["STREAM_EXPIRES_AFTER_SECONDS"]}s`}
     hx-swap="outerHTML"
     class="hidden"
