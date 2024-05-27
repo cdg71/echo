ea0bdb6bb06d13d313b4260f3652e254f872db1d
jpbourgeon <3356641+jpbourgeon@users.noreply.github.com>
2024-05-27 21:40:51 +0200

    feat(backend): heroicons loader

---
+++ src/libs/heroIconsLoader.ts
@@ -0,0 +1,68 @@
+import fs from "fs";
+
+interface Props {
+  iconName: string;
+  family?: "Outline" | "Solid" | "Mini" | "Micro";
+  className?: string;
+}
+
+const defaultProps = {
+  className: "size-8",
+  family: "Outline",
+};
+
+// Function to load SVG file and replace class attribute with provided className
+export const heroIconsLoader = (props: Props): string => {
+  const { iconName, family, className } = { ...defaultProps, ...props };
+  let familyPath = "";
+  switch (family) {
+    case "Micro": // Micro : 16x16, Solid fill
+      familyPath = "16/solid";
+      break;
+
+    case "Mini": // Mini : 20x20, Solid fill
+      familyPath = "20/solid";
+      break;
+
+    case "Outline": // Outline : 24x24, 1.5px stroke
+      familyPath = "24/outline";
+      break;
+
+    case "Solid": // Solid : 24x24, Solid fill
+      familyPath = "24/solid";
+      break;
+
+    default:
+      const _exhaustiveCheck = family as never;
+      throw new Error(`Unhandled family: ${_exhaustiveCheck}`);
+  }
+  // Define the path to the SVG file based on the family
+  const filePath = `${process.cwd()}/node_modules/heroicons/${familyPath}/${iconName}.svg`; // Adjust the path as needed
+
+  try {
+    // Load SVG file as string
+    let svgContent = fs.readFileSync(filePath, "utf8");
+
+    // Replace class attribute with provided className, or append it if className is provided
+    if (className) {
+      // Match the existing class attribute
+      const classRegex = /class="([^"]*)"/g;
+      const match = classRegex.exec(svgContent);
+      if (match) {
+        // If class attribute exists, append className to it
+        const existingClass = match[1];
+        const newClass = `${existingClass} ${className}`;
+        // Replace the existing class attribute with the modified one
+        svgContent = svgContent.replace(classRegex, `class="${newClass}"`);
+      } else {
+        // If class attribute does not exist, simply add className
+        svgContent = svgContent.replace(/<svg/, `<svg class="${className}"`);
+      }
+    }
+
+    return svgContent;
+  } catch (error) {
+    console.error(`Error loading SVG file ${family}:`, error);
+    return ""; // Return empty string in case of error
+  }
+};
--- todo.md
+++ todo.md
@@ -1,6 +1,23 @@
 # Todo
 
+- categories: feat: feature, feat(backend): backend feature, feat(frontend): frontend feature, fix: bugfix, docs: documentation, dev: developer environment, refactor: refactoring, perf: performance, test: test
+
 ## sprintlog
+
+- [-] feat(backend): appshell
+- [x] feat(backend): heroicons loader
+- [x] feat(frontend): navbar
+- [-] feat: "administrer un sondage" (Fonctionnalité principale du sprint)
+  - [-] "Nouveau sondage"
+    - [ ] Page
+    - [ ] Formulaire
+    - [ ] Validation client
+    - [ ] Validation serveur
+    - [ ] Enregistrement en base et redirection vers page d'administration avec affichage du "code de sécurité"
+  - [ ] "configurer un sondage"
+  - [ ] "supprimer un sondage"
+- [ ] backend: bun sqlite
+- [ ] backend: rendre la Base de données accessible par le store Elysia
 
 ## Backlog
 
@@ -8,7 +25,6 @@
 
   - [ ] Authentification : utiliser cookies / JWT signés pour identifier la session, l'utilisateur via son profil et gérer les ressources dynamiquement (streams, styles, etc.)
   - [ ] Mentions légales : politique de confidentialité
-  - [ ] Page : créer un sondage
   - [ ] Page : personnaliser un sondage
   - [ ] Page : participer à un sondage - accueil : si absence de profil = page profil ; si profil mais pas de résultat = sondage et commentaires ; si profil et résultat = page résultat
   - [ ] Page : participer à un sondage - profil personnalisé
@@ -18,12 +34,11 @@
 
 - Backend
 
-  - [ ] Base de données : bun sqlite
-  - [ ] Base de données => accessible par le store Elysia ?
   - [ ] Architecture applicative : Effect + grokking simplicity
 
 - Environnement de développement
 
+  - [ ] dev: Live Reload backend / frontend pour une meilleur expérience de développement (NODE_ENV = development + bun --watch + xh header + SSE Trigger Server Callbacks(script nonce + location.reload()))
   - [ ] CI/CD
     - [ ] Continuous Integration : Github actions : typecheck, lint, test, enforce conventional commit in the PR name and new changesets
     - [ ] Continuous delivery : Github release (changeset version and publish) + publish to ghcr.io
