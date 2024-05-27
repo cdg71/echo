40e8670ca21c77ea11cd11eaa38bd9a87ecfa4ef
jpbourgeon <3356641+jpbourgeon@users.noreply.github.com>
2024-05-27 22:20:47 +0200

    feat: manage a survey - page - form

--- src/services/editSurvey/index.tsx
+++ src/services/editSurvey/index.tsx
@@ -1,28 +1,58 @@
 import { Elysia } from "elysia";
-import { htmlTemplate } from "@src/components/htmlTemplate";
+import { appShell } from "@src/components/appShell";
 
 interface Props {
   surveyId?: string;
   adminToken?: string;
 }
 
-// const { surveyId, adminToken } = props;
 const editSurveyComponent = (props: Props) => {
   const createNewSurvey = props.adminToken ? true : false;
-  const title = createNewSurvey ? "Modifier le sondage" : "Nouveau sondage";
-  return (
-    <div class="prose">
-      <h1>{title}</h1>
-    </div>
+  const title = createNewSurvey ? props.surveyId : "Nouveau sondage";
+  const content = (
+    <form class="w-full max-w-lg space-y-4">
+      <label class="form-control">
+        <input
+          type="text"
+          placeholder="Nom du sondage"
+          class="input input-bordered"
+        />
+      </label>
+      <label class="form-control">
+        <input
+          type="text"
+          placeholder="Code du sondage"
+          class="input input-bordered"
+        />
+        <div class="label pt-0">
+          <span class="label-text-alt">
+            Uniquement des lettres, des chiffres et des tirets "-"
+          </span>
+        </div>
+      </label>
+      <label class="form-control">
+        <textarea
+          class="textarea textarea-bordered h-36"
+          placeholder="Description du sondage"
+        ></textarea>
+      </label>
+      <button
+        hx-get="/create"
+        hx-trigger="click"
+        hx-swap="none"
+        class="btn btn-primary"
+      >
+        Créer un sondage
+      </button>
+    </form>
   );
+  return appShell({ title, content });
 };
 
-export const editSurveyService = new Elysia().get("/create", () => {
-  return htmlTemplate({
-    children: editSurveyComponent({}),
-  });
+export const editSurveyService = new Elysia().get("/new", () => {
+  return editSurveyComponent({});
   // .get("/:surveyId/:adminToken", ({ params: {surveyId, adminToken} }) => {
   //   return htmlTemplate({
-  //     children: editSurveyComponent({ surveyId, adminToken }),
+  //     content: editSurveyComponent({ surveyId, adminToken }),
   //   });
 });
