e9e0480a0ac857f54c0ff7714f98a161d3afbd27
jpbourgeon <3356641+jpbourgeon@users.noreply.github.com>
2024-05-27 22:11:07 +0200

    refactor: remove useless state from index page

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,12 @@ import { cors } from "@elysiajs/cors";
 import { helmet } from "elysia-helmet";
 import { tailwind } from "@gtramontina.com/elysia-tailwind";
 import { staticPlugin } from "@elysiajs/static";
-import { streamsStore } from "@src/services/streams/store";
 import { htmxPlugin } from "@src/plugins/htmx";
 import { homepageService } from "@src/services/homepage";
 import { streamsService } from "@src/services/streams";
 import { editSurveyService } from "./services/editSurvey";
 
 new Elysia()
-  .use(streamsStore)
   .use(cors())
   .use(helmet())
   .use(staticPlugin())
